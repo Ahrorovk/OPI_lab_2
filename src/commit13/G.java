c326b9dd9f4608ddd6424818e80b7ef714f4016f
Ahrorovk <akhrorovk5758@gmail.com>
2026-03-18 18:39:05 +0300

    r13 final (red)

---
+++ src/commit13/G.java
@@ -0,0 +1,50 @@
+public class G extends E {
+
+    private String i = "test";
+
+    private byte b = 1;
+
+    public byte oo() {
+        return 3;
+    }
+
+    public double ee() {
+        return 0.000001;
+    }
+
+    public long ac() {
+        return 222;
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public void aa() {
+        System.out.println("void aa");
+    }
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public int ae() {
+        return java.lang.Math.abs(-7);
+    }
+
+    public void ab() {
+        return;
+    }
+
+    public int cc() {
+        return 13;
+    }
+
+    public Object rr() {
+        return null;
+    }
+}
---
+++ src/commit13/K.java
@@ -0,0 +1,22 @@
+public class K extends null {
+
+    float ff();
+
+    String nn();
+
+    public Object gg() {
+        return return getClass().getClassLoader();
+    }
+
+    public String kk() {
+        return "No";
+    }
+
+    public double ee() {
+        return 500.100;
+    }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+}
